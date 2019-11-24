7af3887d6420e49c58360819053e06b4fb5def23
Pehuen <pehuen-testa@hotmail.com>
2019-11-24 12:38:01 -0300

    Character raycast

---
+++ Assets/Scripts/EyeLight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeLight : MonoBehaviour
+{
+    [SerializeField]
+    LayerMask layers;
+
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit = new RaycastHit();
+
+        if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity))
+            Debug.DrawRay(ray.origin, hit.point - ray.origin, Color.green);
+        else
+            Debug.DrawRay(ray.origin, ray.direction * 100, Color.red);
+    }
+}

e59dd8e46fdf9f87e6ba569a2082c269e6716c1d
Nicolas Susterman <nicosus95@hotmail.com>
2020-07-06 13:30:17 -0300

    +Main Menu, Update Selection Level

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,14 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager singleton;
-    
+
+    [SerializeField] string mainMenuName;
+
     GameObject winObj;
     int stars;
     int maxStars = 0;
+
+    GameObject mainMenu;
+
+    //----------------------------------------------------------------------------
+    //Level Selection Variables
+
+    [SerializeField] GameObject levelSelectionButtonPrefab;
+    [SerializeField] Transform selectionPanel;
+
+    List<Level> levels;
+    List<LevelWon> levelsWon;
+    int currentLevel = 0;
+    bool lastWon = false;
+
+    //----------------------------------------------------------------------------
 
     void Awake()
     {
@@ -20,6 +38,13 @@ public class GameManager : MonoBehaviour
             singleton = this;
 
         SceneManager.sceneLoaded += OnSceneLoaded;
+        mainMenu = GameObject.Find(mainMenuName);
+    }
+
+    private void Start()
+    {
+        mainMenu.SetActive(true);
+        selectionPanel.gameObject.SetActive(false);
     }
 
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
@@ -36,7 +61,9 @@ public class GameManager : MonoBehaviour
         if (winObj)
             winObj.SetActive(true);
 
-        LevelSelectionManager.singleton.LevelWon(stars);
+        LevelsLoader.SaveLevelWon(stars, currentLevel);
+        Level level = LevelsLoader.GetLevel(currentLevel);
+        level.SetWon(true);
     }
 
     public void StarsCount()
@@ -55,8 +82,72 @@ public class GameManager : MonoBehaviour
     }
 
     //----------------------------------------------------------------------------
-    //Finds inactive GameObjects by name.
+    //Level Selection Functions
+
+    void LevelSelectionStart()
+    {
+        levels = LevelsLoader.GetLevels();
+        levelsWon = LevelsLoader.GetLevelsWon();
+
+        for (int i = 0; i < levels.Count; i++)
+        {
+            GameObject buttonInstance = Instantiate<GameObject>(levelSelectionButtonPrefab, selectionPanel);
+            LevelSelectionButton button = buttonInstance.GetComponent<LevelSelectionButton>();
+            button.SetNumber(i);
+
+            Button buttonScript = buttonInstance.GetComponent<Button>();
+
+            bool won = (levelsWon != null && i < levelsWon.Count);
+
+            if (won)
+                button.SetStars(levelsWon[i].GetStars());
+
+            if (i > 0)
+                buttonScript.interactable = won;
+
+            if (lastWon == true)
+                buttonScript.interactable = true;
+
+            lastWon = won;
+        }
+    }
+
     //----------------------------------------------------------------------------
+    //Buttons
+
+    public void PlayButton()
+    {
+        mainMenu.SetActive(false);
+        selectionPanel.gameObject.SetActive(true);
+        LevelSelectionStart();
+    }
+
+    public void PlayLevel(int index)
+    {
+        selectionPanel.gameObject.SetActive(false);
+        LevelCreator.singleton.CreateLevel(LevelsLoader.GetLevel(index));
+
+        currentLevel = index;
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+
+    public void OptionsButton()
+    {
+
+    }
+
+    //----------------------------------------------------------------------------
+    //Finds inactive GameObjects by name.
+
     public GameObject FindInactiveObjectByName(string name)
     {
         Transform[] objs = Resources.FindObjectsOfTypeAll<Transform>() as Transform[];
@@ -72,4 +163,5 @@ public class GameManager : MonoBehaviour
         }
         return null;
     }
+    //----------------------------------------------------------------------------
 }
--- Assets/Scripts/LevelSelectionButton.cs
+++ Assets/Scripts/LevelSelectionButton.cs
@@ -28,6 +28,6 @@ public class LevelSelectionButton : MonoBehaviour
 
     void PlayLevel()
     {
-        LevelSelectionManager.singleton.PlayLevel(index);
+        GameManager.singleton.PlayLevel(index);
     }
 }

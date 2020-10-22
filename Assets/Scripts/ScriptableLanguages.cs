53119458c62ffcd7da6956c48ddaabaecaf5864c
Nicolas Susterman <nicosus95@hotmail.com>
2020-10-22 13:28:52 -0300

    Text UI icons deleted

--- Assets/Scripts/ScriptableLanguages.cs
+++ Assets/Scripts/ScriptableLanguages.cs
@@ -5,24 +5,14 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Language")]
 public class ScriptableLanguages : ScriptableObject
 {
-    [SerializeField] string playText;
     [SerializeField] string optionsText;
     [SerializeField] string exitText;
 
     [SerializeField] string winText;
-    [SerializeField] string nextLevel;
-    [SerializeField] string playAgain;
-    [SerializeField] string backToMenu;
-    [SerializeField] string starText;
-    [SerializeField] string maxStarsText;
 
     [SerializeField] string pauseTitle;
-    [SerializeField] string continueText;
-    [SerializeField] string restartText;
 
     [SerializeField] string quitWarningTitle;
-    [SerializeField] string yesText;
-    [SerializeField] string noText;
     
     [SerializeField] string privacyPoliciesText;
     [SerializeField] string muteText;
@@ -48,34 +38,9 @@ public class ScriptableLanguages : ScriptableObject
         return quitWarningTitle;
     }
 
-    public string GetYes()
-    {
-        return yesText;
-    }
-
-    public string GetNo()
-    {
-        return noText;
-    }
-
     public string GetPauseTitle()
     {
         return pauseTitle;
-    }
-
-    public string GetContinueText()
-    {
-        return continueText;
-    }
-
-    public string GetRestartText()
-    {
-        return restartText;
-    }
-
-    public string GetPlayText()
-    {
-        return playText;
     }
 
     public string GetOptionsText()
@@ -91,30 +56,5 @@ public class ScriptableLanguages : ScriptableObject
     public string GetWinText()
     {
         return winText;
-    }
-
-    public string GetNextLevelText()
-    {
-        return nextLevel;
-    }
-
-    public string GetPlayAgainText()
-    {
-        return playAgain;
-    }
-
-    public string GetBackToMenuText()
-    {
-        return backToMenu;
-    }
-
-    public string GetStarsText()
-    {
-        return starText;
-    }
-
-    public string GetMaxStarsText()
-    {
-        return maxStarsText;
     }
 }
